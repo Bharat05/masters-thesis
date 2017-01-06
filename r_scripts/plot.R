863544696e172f921b931e54ba47d7720a8c3530
Bharat05 <bharat.thakur90@yahoo.com>
2017-01-06 14:00:12 +0530

    created

---
+++ r_scripts/plot.R
@@ -0,0 +1,21 @@
+#plotting
+library(ggplot2)
+library(dplyr)
+
+df <- read.csv("C:/Stuff/Google Drive/Declines/df_pca.csv", 
+                 header = T, stringsAsFactors = FALSE)
+
+#plotting clean data,individual plots
+for( i in unique(df$leaseNumber)){
+  temp_data = filter(df, df$leaseNumber == i )
+  #temp_data <- filter(df_gas, df_gas$leaseNumber == i )
+
+
+  ggplot( aes(month_id, gas_norm), data = temp_data) + geom_point(color = "blue") + 
+        labs(title = paste(temp_data$leaseNumber,"lease number", sep =" "))
+  ggsave(filename=paste("C:/Stuff/Google Drive/Declines/production_lease_number",
+                        i,".jpeg",sep=" "), dpi = 200)
+
+  # print(qual$sd)
+
+}
